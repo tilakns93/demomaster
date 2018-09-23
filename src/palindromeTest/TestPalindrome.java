8204df46c57249e3fd878a8f6305c35098e0e820
admin <admin@admin-PC>
2018-09-23 23:06:25 +0530

    PalindromeTestCode

--- src/palindromeTest/TestPalindrome.java
+++ src/palindromeTest/TestPalindrome.java
@@ -1,5 +1,28 @@
 package palindromeTest;
 
+import java.util.*;
+
+
 public class TestPalindrome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the input string:");
+		String reverse = "";
+		Scanner input = new Scanner(System.in);
+		String original = input.nextLine();
+		
+		for(int i=original.length()-1;i>=0;i--) {
+			reverse = reverse+original.charAt(i);
+		}
+		
+		if(original.equalsIgnoreCase(reverse)) {
+			System.out.println("Entered string is a Palindrome");
+		}
+		else {
+			System.out.println("Entered string is not a Palindrome");
+		}
+		
+	}
 
 }
